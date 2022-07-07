91f5ff2c585950058c25f2ec97905713b0bc8b67
USER <USER@DESKTOP-HTLJ4VL>
2022-07-07 23:44:51 +0900

    only commit2

--- src/gitTest/test.java
+++ src/gitTest/test.java
@@ -4,6 +4,7 @@ public class test {
 	public void test() {
 		System.out.println("start");
 		//branch change
-//		change2 from ide
+//		change2 from ide - commit1
+//		change2 from ide - commit2
 	}
 }
